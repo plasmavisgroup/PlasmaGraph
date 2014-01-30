de4301471cf70a604ce057c30610207c74a1fcc8
danielquintini <danielquintini@gmail.com>
2014-01-30 11:40:34 -0400

    Started DataColum class

--- tests/JMatIO-041212/test/com/jmatio/test/main.java
+++ tests/JMatIO-041212/test/com/jmatio/test/main.java
@@ -8,12 +8,15 @@ package com.jmatio.test;
 
 import com.jmatio.io.MatFileReader;
 import com.jmatio.types.MLArray;
+import com.jmatio.types.MLCell;
 import com.jmatio.types.MLJavaObject;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Iterator;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -22,18 +25,27 @@ import static org.junit.Assert.assertEquals;
  */
 public class main {
     
-    static java.lang.String _PROJECTS_PATH = "C:\\Users\\Administrator\\Desktop\\JMatIO-041212\\";
+    static java.lang.String _PROJECTS_PATH = "C:\\wamp\\www\\PlasmaGraph\\tests\\JMatIO-041212\\";
     
     public static void main(String[] args) {
         System.out.println("Test: reading ExperimentData_04_09_2013_dummy");
         MatFileReader mfr = new MatFileReader();
         try {
-            Map <String, MLArray> content = mfr.read( new File(_PROJECTS_PATH + "test/ExperimentData_04_09_2013_dummy.mat"));
+            Map <String, MLArray> dataMap = mfr.read( new File(_PROJECTS_PATH + "test\\ExperimentData_04_09_2013_dummy.mat"));
             
             System.out.println("Size:");
-            System.out.println(content.size());
+            System.out.println(dataMap.size());
             System.out.println("values");
-            System.out.println(content.toString());           
+            System.out.println(dataMap.toString());    
+            
+            for (Iterator iterator = dataMap.values().iterator(); iterator.hasNext();){
+                MLCell cells = (MLCell) iterator.next();
+                //System.out.println(cells.getM());
+                for(int i = 0; i < cells.getN(); i++){
+                    for(int j = 0; j < cells.getM(); j++)
+                        System.out.println(cells.get(j, i));
+                }
+            }
 
         } catch (IOException ex) {
             Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
